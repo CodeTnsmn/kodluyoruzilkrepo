d41e5e36ffa3ce9900336f33a6472a21cd4da74c
tnsmn25 <107807478+tnsmn25@users.noreply.github.com>
2022-06-25 15:21:17 +0300

    DaireCap
    çap hesaplandı.

---
+++ DaireCap.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DaireCap {
+    public static void main(String[] args) {
+
+        int r ;
+        double pi = 3.14 ;
+        double a ;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("dairenin yaricapi : ");
+
+        r = scanner.nextInt();
+
+        Scanner scannera = new Scanner(System.in);
+
+        System.out.println("aci giriniz: ");
+
+        a = scannera.nextDouble() ;
+
+        double alan = pi*r*r ;
+        double cevre = 2 * pi * r;
+        double merkezAlan = (pi*(r*r)*a);
+
+        System.out.println("alan : "+ alan);
+        System.out.println("cevre: "+ cevre);
+        System.out.println("Daire dilimi alani: "+ merkezAlan);
+
+
+    }
+}

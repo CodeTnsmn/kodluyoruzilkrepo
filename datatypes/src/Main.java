6742e675f11697e4b6e10a2f5f9219187ee9b812
tnsmn25 <107807478+tnsmn25@users.noreply.github.com>
2022-09-18 18:43:48 +0300

    datatypes
    data tipleri çalışıldı.

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi1 = 23424;
+        String mesaj = "kodlama";
+        long sayi3 = 345456334;
+        char karakter = 'A';
+        boolean dogruMu= true;
+        double sayi6 =232313.2323233;
+        float sayi7 = 17.323f;
+        byte sayi = -23;
+
+        System.out.println(dogruMu);
+
+    }
+}

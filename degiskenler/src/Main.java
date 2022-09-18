6569d9af6e5ac2bbf72c53d0afb423e107a8bcaa
tnsmn25 <107807478+tnsmn25@users.noreply.github.com>
2022-09-18 18:44:16 +0300

    degiskenler
    değişkenler çaşılışıldı.

---
+++ degiskenler/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayım : ";
+
+        System.out.println("Hello Community " + mesaj +ogrenciSayisi);
+        System.out.println("Hello world " + mesaj + ogrenciSayisi);
+    }
+}

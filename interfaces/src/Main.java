a203b9f6866182bf68fc4944188677e137a7d1af
tnsmn25 <107807478+tnsmn25@users.noreply.github.com>
2022-10-18 20:48:01 +0300

    interfacesDemo
    interfacesDemo

--- interfaces/src/Main.java
+++ interfaces/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        ICustomerDal customerDal = new OracleCustomerDal();
-        customerDal.add();
+        CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+        customerManager.add();
     }
 }
